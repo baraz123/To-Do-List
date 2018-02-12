4acbf864ea8ab100ef74894702e0ba6e9a75de13
bar azoulay <barazoulay92@gmail.com>
2018-02-12 15:26:18 +0200

    commit

--- src/com/hit/model/JDBC.java
+++ src/com/hit/model/JDBC.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 public class JDBC
 {
     public static String driver = "com.mysql.jdbc.Driver";
-    public static String protocol = "jdbc:mysql://localhost:8889/todo";
+    public static String protocol = "jdbc:mysql://localhost:8889/username";
     public static void main(String[] args)
     {
         Connection connection = null;
@@ -20,7 +20,7 @@ public class JDBC
             //this driver as an available driver for DriverManager
             Class.forName(driver);
             //Getting a connection by calling getConnection
-            connection = DriverManager.getConnection(protocol, "todo", "bar1234");
+            connection = DriverManager.getConnection(protocol, "username", "password");
             statement = connection.createStatement();
 
             rs = statement.executeQuery(
